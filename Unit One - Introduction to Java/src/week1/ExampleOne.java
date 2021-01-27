65b3d0eb76c2afdc5eaec9ff96f674df71b55c2a
Marko Dzepina <mdzepina@bayviewglen.ca>
2021-01-27 10:03:16 -0500

    Println and Escape Sequences

--- Unit One - Introduction to Java/src/week1/ExampleOne.java
+++ Unit One - Introduction to Java/src/week1/ExampleOne.java
@@ -1,8 +1,22 @@
 package week1;
 
+// ExampleOne is the name of the Class. It has the same name as the file.
+// Classes are meant to model an object's behaviour.
+// Naming convention for classes is upper case on the first letter of each word.
+// This is a comment - Java compiler ignores comments (everything after //).
+// Comments are used to describe what code is meant to do.
+// It is meant for other programmers.
+// Don't pollute your code with comments - only comment code that is not self-explanatory.
+
+/*
+ *This is a block comment.
+ *
+ */
 public class ExampleOne {
-    
     public static void main(String[] args) {
-        System.out.println("welcome to ICS3U...");
+        System.out.println("Welcome to ICS3U...");
+        System.out.print("This is another line that does not end with a new line."); // Print does not add a new line after it writes to the console
+        System.out.println("This is another line!");
+
     }
 }
---
+++ Unit One - Introduction to Java/src/week1/ExampleTwo.java
@@ -0,0 +1,21 @@
+package week1;
+
+/*
+* Escape Sequences
+*/
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("This is very \"exciting\" code"); // "Quotes" identify the beggining and end of a string literal - text you want to include in the code.
+                                                                // backslash \ identifies an escape sequence, it creates a special character.
+
+        System.out.println("This is an error");              // We can only escape certain chraracters (AP escape sequences are \n, \"", \\).
+
+        System.out.println("This\nis\na\nvalid\nescape\nsequence.\n");
+
+    }
+
+    
+}
+
+
